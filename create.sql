3cea3b187e1930aa09599e08d76d7b783269a6bd
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2023-11-16 18:39:11 +0100

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS Benutzer (
-    name VARCHAR(255),
+    vorname VARCHAR(255),
     nachname VARCHAR(255),
     email VARCHAR(250)
 );
 
-INSERT INTO Benutzer (name, nachname, email) VALUES ('Hans', 'Dampf', 'hans@dampf.de');
+INSERT INTO Benutzer (vorname, nachname, email) VALUES ('Hans', 'Dampf', 'hans@dampf.de');
 
 
-INSERT INTO Benutzer (name, nachname, email) VALUES ('Lilli', 'Wolke', 'lilli@wolke.de');
+INSERT INTO Benutzer (vorname, nachname, email) VALUES ('Lilli', 'Wolke', 'lilli@wolke.de');
 
 
-UPDATE Benutzer SET nachname = 'Dampf-Wolke' WHERE (name = 'Hans' AND nachname = 'Dampf' AND email = 'hans@dampf.de') OR (name = 'Lilli' AND nachname = 'Wolke' AND email = 'lilli@wolke.de');
+UPDATE Benutzer SET nachname = 'Dampf-Wolke' WHERE (vorname = 'Hans' AND nachname = 'Dampf' AND email = 'hans@dampf.de') OR (vorname = 'Lilli' AND nachname = 'Wolke' AND email = 'lilli@wolke.de');
 
